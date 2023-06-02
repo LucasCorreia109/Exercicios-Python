86cdedea821af298bddb0d402d6c977bba518503
LucasCorreia109 <lucasmcorreia109@gmail.com>
2023-06-02 14:52:11 -0300

    atualização

---
+++ exercicio011.py
@@ -0,0 +1,3 @@
+real = float(input('Quantos reais você tem na carteira?'))
+dolar = real / 5.02
+print('Com R$ {} reais você pode comprar US$ {:.2f} dólares. \nCoração do dia 01/06/23'.format(real, dolar))
---
+++ modulos/exericicio017.py
@@ -0,0 +1,4 @@
+import math
+num = int(input('Digite um numero para ver a raiz quadrada: '))
+raiz = math.sqrt(num)
+print('A raiz quadrada de {} é igual a {:.2f}'.format(num, raiz))
